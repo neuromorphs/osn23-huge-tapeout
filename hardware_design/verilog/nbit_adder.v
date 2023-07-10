f6fa6fc2cfe921d80cf193b8f07c6ce78b9e74d0
Andrew Wabnitz <andywabnitz@gmail.com>
2023-07-10 17:39:41 -0600

    Corrected ChatGPT errors in N-bit adder

--- hardware_design/verilog/nbit_adder.v
+++ hardware_design/verilog/nbit_adder.v
@@ -1,32 +1,36 @@
-module nbit_adder #(parameter n = 4) (
+module nbit_adder #(
+    parameter n = 4
+)
+(
     input [(n-1):0] A,
     input [(n-1):0] B,
     output [n:0] S
 );
-    // Define internal signals
-    logic [n:0] Cout, Cin;
 
-    // Generate full adders and half adders
+    wire [n-1:0] Cout;
+
+    // Bit0 -> half adder
+    half_adder ha0 (
+        .A(A[0]),
+        .B(B[0]),
+        .S(S[0]),
+        .Cout(Cout[0])
+    );
+
+    // Bit1:n -> full adders
+    genvar i;
     generate
-        genvar i;
-        for (i=0; i<n; i=i+1) begin : adder_i
-            if (i == 0) begin : first_ha
-                half_adder ha1 (
-                    .A(A[i]),
-                    .B(B[i]),
-                    .S(S[i]),
-                    .Cout(Cout[i])
-                );
-            end else begin : other_fa
-                full_adder other_fa_i (
-                    .A(A[i]),
-                    .B(B[i]),
-                    .Cin(Cout[i-1]),
-                    .S(S[i]),
-                    .Cout(Cout[i])
-                );
-            end
-        end
+    for (i = 1; i < n; i = i+1) begin
+        full_adder faI (
+            .A(A[i]),
+            .B(B[i]),
+            .Cin(Cout[i-1]),
+            .S(S[i]),
+            .Cout(Cout[i])
+        );
+    end
     endgenerate
+
+    assign S[n] = Cout[n-1];
 
 endmodule
